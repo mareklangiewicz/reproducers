b79ed34e7afd068adec8e09e52982b6bec0bd023
Marek Langiewicz <marek.langiewicz@gmail.com>
2024-06-10 10:58:44 +0200

    fun

--- reproduce-05-k2js-vararg/app/src/jsMain/kotlin/Main.js.kt
+++ reproduce-05-k2js-vararg/app/src/jsMain/kotlin/Main.js.kt
@@ -1,22 +1,7 @@
-package pl.mareklangiewicz.reproduce03
+package pl.mareklangiewicz.reproduce05
 
-import org.jetbrains.compose.web.*
-import org.jetbrains.compose.web.dom.*
 import androidx.compose.runtime.*
-
-
 
 @Composable fun <T> MySwitch(state: MutableState<T>, vararg options: Pair<String, T>) {
 }
 
-fun main() {
-    console.log("reproduce05.main started.")
-    console.log("Kotlin version: ${KotlinVersion.CURRENT}")
-
-
-    renderComposableInBody {
-        val myState = remember { mutableStateOf(0) }
-        Text("Some not important text")
-        MySwitch(myState)
-    }
-}
