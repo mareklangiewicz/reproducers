535cdb66c351b48b3d234ac24054b9622347c6ed
Marek Langiewicz <marek.langiewicz@gmail.com>
2024-06-10 10:53:32 +0200

    fun

--- reproduce-05-k2js-vararg/app/build.gradle.kts
+++ reproduce-05-k2js-vararg/app/build.gradle.kts
@@ -26,7 +26,6 @@ kotlin {
         val jsMain by getting {
             dependencies {
                 implementation(compose.runtime)
-                implementation(compose.ui)
                 implementation(compose.html.core)
             }
         }
