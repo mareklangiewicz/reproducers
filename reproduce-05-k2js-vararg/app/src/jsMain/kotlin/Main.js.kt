d2f1d9166189f1999ab279aa9a2376779d4d7a51
Marek Langiewicz <marek.langiewicz@gmail.com>
2024-06-10 11:03:26 +0200

    fun

--- reproduce-05-k2js-vararg/app/src/jsMain/kotlin/Main.js.kt
+++ reproduce-05-k2js-vararg/app/src/jsMain/kotlin/Main.js.kt
@@ -2,6 +2,6 @@ package pl.mareklangiewicz.reproduce05
 
 import androidx.compose.runtime.*
 
-@Composable fun <T> MySwitch(state: MutableState<T>, vararg options: Pair<String, T>) {
+@Composable fun <T> MySwitch(vararg options: Pair<String, T>) {
 }
 
