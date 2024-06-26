3ffdfc587eaf366b1d50316cc19f93605462c882
Marek Langiewicz <marek.langiewicz@gmail.com>
2024-06-26 12:23:16 +0200

    fun

--- reproduce-06-compose-html/build.gradle.kts
+++ reproduce-06-compose-html/build.gradle.kts
@@ -2,8 +2,8 @@
 plugins {
     kotlin("multiplatform") version "2.0.20-Beta1" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.20-Beta1" apply false
-    id("org.jetbrains.compose") version "1.7.0-dev1686" apply false // works correctly with this version
-    // id("org.jetbrains.compose") version "1.7.0-dev1698" apply false // reproduces with this version
+    // id("org.jetbrains.compose") version "1.7.0-dev1686" apply false // works correctly with this version
+    id("org.jetbrains.compose") version "1.7.0-dev1698" apply false // reproduces with this version
 }
 
 group = "pl.mareklangiewicz"
